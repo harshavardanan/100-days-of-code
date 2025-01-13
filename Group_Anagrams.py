fe2aa98251b436cbe805e79ad26b6264ab1cd4bd
harshavardanan <harshamoorthy22@gmail.com>
2025-01-13 19:20:30 +0000

    fixing required

---
+++ Group_Anagrams.py
@@ -0,0 +1,21 @@
+def findAnagrams(strs):
+    output = []
+    a = 0
+    ans = []
+    for i in strs:
+        chars = {a : i.count(a) for a in i}
+        output.append(chars)
+    for i in range(len(output)):
+        similar = []
+        if output[i] not in ans:
+            similar.append(output[i])
+            for j in range(i+1, len(output)):
+                if output[i]==output[j]:
+                    ans.append(output[j])
+                else:
+                    ans.append(output[j])
+            print(similar)
+        ans.append(similar)
+    return ans
+
+print(findAnagrams(["act","pots","tops","cat","stop","hat"]))
