b5078339b1973f71536a45259afaf49cfda1dccc
harshavardanan <harshamoorthy22@gmail.com>
2025-01-14 17:22:38 +0000

    Group anagrams

--- Group_Anagrams.py
+++ Group_Anagrams.py
@@ -1,21 +1,22 @@
 def findAnagrams(strs):
     output = []
-    a = 0
     ans = []
     for i in strs:
         chars = {a : i.count(a) for a in i}
         output.append(chars)
     for i in range(len(output)):
         similar = []
-        if output[i] not in ans:
-            similar.append(output[i])
-            for j in range(i+1, len(output)):
-                if output[i]==output[j]:
-                    ans.append(output[i])
-                else:
-                    ans.append(output[j])
-            print(similar)
-        ans.append(similar)
+        similar.append(strs[i])
+        for j in range(i+1, len(output)):
+            if output[i]==output[j]:
+                similar.append(strs[j])
+        if any(similar[0] in sublist for sublist in ans):
+            pass
+        else:
+            ans.append(similar)
+            similar = []
     return ans
 
+
 print(findAnagrams(["act","pots","tops","cat","stop","hat"]))
+
